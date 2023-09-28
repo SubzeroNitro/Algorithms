3dc2125a56e36c2e2e09d0939b32feeb316e31c8
David Villa <DRVillaSE@gmail.com>
2023-09-28 10:45:15 -0400

    SortArrayByParity on Leetcode

---
+++ Leetcode/C++/SortArrayByParity.hpp
@@ -0,0 +1,23 @@
+#include <vector>
+
+std::vector<int32_t> sortArrayByParity(std::vector<int32_t>& nums)
+{
+    std::vector<int32_t> results(nums.size());
+
+    uint16_t evenIndex = 0;
+    uint16_t oddIndex = nums.size() - 1;
+
+    for (const int32_t num : nums)
+    {
+        if (num & 1)
+        {
+            results[oddIndex--] = num; 
+        }
+        else
+        {
+            results[evenIndex++] = num;
+        }
+    }        
+
+    return results;
+}
