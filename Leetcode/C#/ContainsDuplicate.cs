e98de2bba3ecbbbe46368e64ad0a6d81573f3291
David <drvillase@gmail.com>
2023-10-02 00:50:14 -0400

    ContainsDuplicate in Leetcode

---
+++ Leetcode/C#/ContainsDuplicate.cs
@@ -0,0 +1,18 @@
+public bool ContainsDuplicate(int[] nums)
+{
+    HashSet<int> set = new HashSet<int>();
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        int num = nums[i];
+
+        if (set.Contains(num))
+        {
+            return true;
+        }
+
+        set.Add(num);
+    }
+
+    return false;
+}
